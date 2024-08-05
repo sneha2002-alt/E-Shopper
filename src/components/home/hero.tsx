c0247e5bd7a7dd579e6c53fd6ea068ed63698d83
maahin2005 <mahin.malek2005@gmail.com>
2024-08-05 19:05:53 +0530

    Product--page--and many more...

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -1,7 +1,36 @@
+import { homeData } from "@/utils/homePage";
+import { TabsContent } from "@radix-ui/react-tabs";
 import React from "react";
+import ShowProducts from "../products/showProducts";
+import MainHero from "./mainHero";
+import { productData } from "@/utils/data";
+import { IProductCollection } from "@/types";
+
+type HeroTabs = keyof IProductCollection | "mainHero";
 
 function Hero() {
-  return <div>Hero</div>;
+  const {
+    heroSection: { heroSectionTabsValuesArr },
+  } = homeData;
+
+  const validTabsValuesArr = heroSectionTabsValuesArr as HeroTabs[];
+
+  return (
+    <section className="w-3/4 p-5 max-h-[90vh] overflow-scroll">
+      {validTabsValuesArr.map((value) => (
+        <TabsContent key={value} value={value}>
+          {value === "mainHero" ? (
+            <MainHero />
+          ) : (
+            <ShowProducts
+              dataVal={value}
+              productArr={productData[value as keyof IProductCollection]}
+            />
+          )}
+        </TabsContent>
+      ))}
+    </section>
+  );
 }
 
 export default Hero;
--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,7 +1,33 @@
+import { homeData } from "@/utils/homePage";
+import { TabsList, TabsTrigger } from "@radix-ui/react-tabs";
 import React from "react";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const {
+    sidebar: { heading, catArr },
+  } = homeData;
+  return (
+    <div className="w-1/4 h-[90vh] border-2 text-center font-sans sticky left-0 bottom-0">
+      <div className="grid w-full">
+        <h1 className="p-5 bg-rose-500 w-full font-semibold text-white">
+          {heading}
+        </h1>
+        <div className="w-full mt-1">
+          <TabsList className="w-full">
+            {catArr.map((el, i) => (
+              <TabsTrigger
+                key={i}
+                value={el.value}
+                className={`block border-b-2 hover:bg-rose-500 hover:text-white p-2 w-full`}
+              >
+                {el.title}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
